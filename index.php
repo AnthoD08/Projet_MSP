4adc9358f23e7357d403cf6d6eab27b0e023f0af
Thomas <thomas.simeon36@gmail.com>
2024-12-17 14:16:49 +0100

    modification router

--- index.php
+++ index.php
@@ -4,7 +4,23 @@
     //connexion PDO
     require('./bdd/pdo.php');
     switch ($route[0]){
-        case "scanner": case "":
-            
+        case "scanner":case "":
+            include('./pages/scanner.php');
+            break;
+        case "livre":
+            if(isset($route[1]) && !empty($route[1]) && is_numeric($route[1])){
+                include('./pages/livre.php');
+            }
+            break;
+
+        case "post-scanner":
+            include('./traitement/post-scanner.php');
+            break;
+
+        case "retour":
+            include('./traitement/retour.php');
+            break;
+
+
     }
 ?>
